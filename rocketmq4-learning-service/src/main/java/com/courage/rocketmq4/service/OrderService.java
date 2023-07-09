3a91b9e64e90cb1cb04e0d33914f9e8c7f5acfb3
zhangyong <zhangyong7120180@163.com>
2023-07-09 21:51:59 +0800

    订单事务信息

--- rocketmq4-learning-service/src/main/java/com/courage/rocketmq4/service/OrderService.java
+++ rocketmq4-learning-service/src/main/java/com/courage/rocketmq4/service/OrderService.java
@@ -1,12 +1,14 @@
 package com.courage.rocketmq4.service;
 
 import com.courage.rocketmq4.domain.mapper.OrderMapper;
+import com.courage.rocketmq4.domain.po.OrderPO;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.TransactionMQProducer;
 import org.apache.rocketmq.client.producer.TransactionSendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
@@ -23,5 +25,12 @@ public class OrderService {
         return this.producer.sendMessageInTransaction(message, null);
     }
 
+    @Transactional(rollbackFor = Exception.class)
+    public void createOrder(OrderPO orderPO) {
+        // 插入到订单表
+        orderMapper.insert(orderPO);
+        // 插入到事务日志表
+
+    }
 
 }
